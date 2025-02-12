b906a5203d2cac4a080b5e4bc45650eb8437e1d6
James Burton <jimburton1@gmail.com>
2025-02-12 21:25:48 +0000

    Switching to wtf

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -6,11 +6,13 @@ from flask import (
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from flaskr.db import get_db
+from flaskr.forms import UserForm
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
+    form = UserForm()
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
